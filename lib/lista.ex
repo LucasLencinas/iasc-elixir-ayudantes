b6ecd60d3de464da7d4efb2a86790eed70765fbf
Lucas Lencinas <lllencinas@gmail.com>
2015-10-27 21:58:17 -0300

    fix de id consulta que era el mismo numero

--- lib/lista.ex
+++ lib/lista.ex
@@ -5,41 +5,49 @@ defmodule Lista do
     spawn_link(fn -> loop({[],[],Map.new,1}) end)
   end
 
-  def loop({alumnos,docentes,consultas, id_consulta}) do
+  def loop({alumnos,docentes,consultas, next_id}) do
+    IO.puts "id_consulta: " <> Integer.to_string(next_id)
     receive do
 
       {pid, :alumno, nombre } ->
           send pid, {self, :ok}
-          loop({alumnos ++ [{pid,nombre}],docentes, consultas, id_consulta})
+          loop({alumnos ++ [{pid,nombre}],docentes, consultas, next_id})
 
       {pid, :docente, nombre } ->
           send pid, {self, "docente guardado"}
-          loop({alumnos,docentes ++ [{pid,nombre}], consultas, id_consulta})
+          loop({alumnos,docentes ++ [{pid,nombre}], consultas, next_id})
 
       {pid, :preguntar, pregunta } ->
           IO.puts "pregunta: " <> pregunta
-          broadcast(pids_of(alumnos ++ docentes), {self, :consulta,{id_consulta, pid, pregunta}})
+          broadcast(pids_of(alumnos ++ docentes), {self, :consulta,{next_id, pid, pregunta}})
           loop({alumnos, docentes,
-                Map.put(consultas, id_consulta, {pid,pregunta, nil}),
-                id_consulta + 1 })
+                Map.put(consultas, next_id, {pid,pregunta, nil}),
+                next_id + 1 })
 
       {pid, :respondiendo, id_consulta } ->
           broadcast(pids_of(docentes),{self,:respondiendo, id_consulta, pid})
           IO.puts "respondiendo: " <> Integer.to_string(id_consulta)
-          loop({alumnos, docentes,consultas,id_consulta})
+          loop({alumnos, docentes,consultas,next_id})
 
       {pid, :responder,id_consulta, respuesta } ->
+
           IO.puts "respuesta: " <> respuesta
-          {alumno,pregunta,_} = Map.get(consultas, id_consulta)
-          consulta = {alumno, pregunta, respuesta}
-          broadcast(pids_of(alumnos ++ docentes),{self,:respuesta, id_consulta, consulta})
-          loop({alumnos, docentes,
-                Map.put(consultas, id_consulta, consulta),
-                id_consulta})
+          case Map.get(consultas, id_consulta) do
+            {alumno,pregunta,nil} ->
+              consulta = {alumno, pregunta, respuesta}
+              broadcast(pids_of(alumnos ++ docentes),{self,:respuesta, id_consulta, consulta})
+              loop({alumnos, docentes,
+                    Map.put(consultas, id_consulta, consulta),
+                    next_id})
+            _ ->
+              IO.puts "Ya respondieron"
+              loop({alumnos, docentes,consultas,next_id})
+
+          end
 
       {pid, :consultas} ->
         send pid, consultas
-        loop({alumnos,docentes, consultas, id_consulta})
+        loop({alumnos,docentes, consultas, next_id})
 
     end
   end
